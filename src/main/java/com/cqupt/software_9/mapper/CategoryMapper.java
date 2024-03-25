d82c2c5d9e5a56f506e106e17b48e3a655c3e23e
cp <cp@cqut.com>
2024-03-25 16:18:12 +0800

    整合公共模块-字段管理、rate统计、tabledescrible等

--- src/main/java/com/cqupt/software_9/mapper/CategoryMapper.java
+++ src/main/java/com/cqupt/software_9/mapper/CategoryMapper.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cqupt.software_9.entity.CategoryEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 // TODO 公共模块新增类
 
 @Mapper
+@Repository
 public interface CategoryMapper extends BaseMapper<CategoryEntity> {
     void removeNode(@Param("id") String id);
 }
